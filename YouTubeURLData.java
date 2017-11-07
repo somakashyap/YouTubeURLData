d920546112443f06dd6e0eb50e110661c56af9d1
somakashyap <31776139+somakashyap@users.noreply.github.com>
2017-11-07 13:42:43 -0800

    Uploading new files for related videos
    Uploading new files with an option to see related videos. It also allows to see details about any selected related videos in cycle.

--- YouTubeURLData.java
+++ YouTubeURLData.java
@@ -5,6 +5,8 @@
  */
 package youtubeurldata;
 
+import java.util.Scanner;
+
 /**
  *
  * @author somak
@@ -15,10 +17,48 @@ public class YouTubeURLData {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String url = "https://www.youtube.com/watch?v=GOOAA";
+        Scanner scanner = new Scanner(System.in);
+        String ans = "Y";
+        String url = "https://www.youtube.com/watch?v=GOOAAqe63Pg&t=840s";
         YouTubeURLDataManager youtubeManager = new YouTubeURLDataManager();
-        if (youtubeManager.printVideoData(url) == false){
-            System.out.println("Not a valid URL");
+        while (ans.equals("Y")){
+            if (youtubeManager.inputURL(url) == false){
+                System.out.println("Not a valid URL");
+                System.out.println("Do you want to enter URL (Y/N) :");
+                ans = scanner.next();
+                if (ans.equals("Y")){
+                    System.exit(0);
+                }
+            }
+            else {
+                ans = "N";
+            }
+        }
+        System.out.println("The video data corresponding to URL is");
+        youtubeManager.printVideoData();
+        System.out.println("--------------------------------------------------------------------------");
+        
+        System.out.println("Do you want to see related videos corresponding to above video (Y/N)");
+        
+        ans = scanner.next();
+        if (ans.equals("Y")){
+            youtubeManager.getRelatedVideos();
+        }
+        
+        while (ans.equals("Y")){
+            System.out.println ("Select video serial number if you need data about it.");
+            int choice = scanner.nextInt();
+            if (youtubeManager.printVideoDatafromRelatedVideoOption(choice) == false){
+                System.out.println("Incorrect choice or insufficent data.");
+            }
+            else {
+                System.out.println("--------------------------------------------------------------------------");
+                System.out.println("Do you want to see related videos corresponding to above video (Y/N)");
+                ans = scanner.next();
+                if (ans.equals("Y")){
+                    youtubeManager.getRelatedVideoFromLastRelatedVideoChoice();
+                }
+            }
         }
     }
     
--- YouTubeURLDataManager.java
+++ YouTubeURLDataManager.java
@@ -18,13 +18,23 @@ import com.google.api.services.youtube.YouTube.Videos.List;
 import com.google.api.services.youtube.model.VideoListResponse;
 import com.google.api.services.youtube.YouTubeRequestInitializer;
 import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.SearchResultSnippet;
+import com.google.api.services.youtube.model.ResourceId;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class YouTubeURLDataManager {
     
     private YouTube youtube;
+    private String userYouTubeURL;
+    private String userVideoId;
+    private SearchListResponse lastRelatedVideoSearchResponse;
+    private String lastRelatedVideoId;
     
     YouTubeURLDataManager(){
         
@@ -36,18 +46,60 @@ public class YouTubeURLDataManager {
                 })
                 .setApplicationName("youTubeURLfinder")
                 .setYouTubeRequestInitializer(new YouTubeRequestInitializer("AIzaSyDSPuYKyzystDdUt-aMMpUyKtE4G-5-CjM")).build();
+        
+
                         
     }
     
-    public boolean printVideoData(String url){
+    public boolean inputURL(String url){
         if (url == null || url.length() == 0){
             return false;
         }
-        
         String videoId = getVideoIDFromURL(url);
         if (videoId == null){
             return false;
         }
+        userYouTubeURL = url;
+        userVideoId = videoId;
+        return true;
+        
+    }
+    
+    public void printVideoData(){
+        
+        printVideoDataFromVideoId(userVideoId);
+    }
+    
+    
+    public boolean getRelatedVideos(){
+        
+        return getRelatedVideosFromVideoId(userVideoId);  
+                      
+    }
+    
+    public boolean printVideoDatafromRelatedVideoOption(int option){
+        if (lastRelatedVideoSearchResponse == null || 
+            lastRelatedVideoSearchResponse.isEmpty()){
+            return false;
+        }
+        if (option < 1 || option > lastRelatedVideoSearchResponse.size()){
+            return false;
+        }
+         
+        java.util.List <SearchResult> searchList = lastRelatedVideoSearchResponse.getItems();
+        SearchResult requiredResult = searchList.get(option - 1);
+        ResourceId resourceId = requiredResult.getId();
+        String videoId = resourceId.getVideoId();
+        lastRelatedVideoId = videoId;
+        printVideoDataFromVideoId(videoId);
+        return true;
+    }
+    
+    public boolean getRelatedVideoFromLastRelatedVideoChoice(){
+        return getRelatedVideosFromVideoId(lastRelatedVideoId);
+    }
+    
+    private void printVideoDataFromVideoId(String videoId){
         List videosListByIdRequest;
         try {
             videosListByIdRequest = youtube.videos().list("snippet,contentDetails,statistics,player");
@@ -57,9 +109,35 @@ public class YouTubeURLDataManager {
         } catch (IOException ex) {
             System.out.println("Exception in getting vidoes" + ex.toString());
         }
+    }
+    private boolean getRelatedVideosFromVideoId(String videoId){
+        if (videoId == null || videoId.length() == 0){
+            return false;
+        }
+        try {
+            YouTube.Search.List searchListByIdRequest = youtube.search().list("snippet");
+            searchListByIdRequest.setRelatedToVideoId(videoId);
+            searchListByIdRequest.setType("video");
+            SearchListResponse response = searchListByIdRequest.execute();
+            lastRelatedVideoSearchResponse = response;
+            System.out.println("Related videos are :");
+            
+            java.util.List <SearchResult> searchList = response.getItems();
+            
+            int i = 1;
+            for (SearchResult result: searchList){
+                SearchResultSnippet snippet = result.getSnippet();
+                System.out.println(i + "." + snippet.getTitle());
+                i++;
+                
+            }
+                
+        } catch (IOException ex) {
+            System.out.println("Exception in getting related vidoes" + ex.toString());
+            return false;
+        }
         return true;
     }
-    
     private String getVideoIDFromURL(String url){
         String reg = "(?:youtube(?:-nocookie)?\\.com\\/(?:[^\\/\\n\\s]+\\/\\S+\\/|(?:v|e(?:mbed)?)\\/|\\S*?[?&]v=)|youtu\\.be\\/)([a-zA-Z0-9_-]{11})";
         Pattern pattern = Pattern.compile(reg, Pattern.CASE_INSENSITIVE);
